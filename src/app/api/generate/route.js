d26ab558050dcf3938fc683dac0fc5d98cf53488
Adeeb Momin <adeebmomin786@gmail.com>
2024-10-18 18:24:47 -0500

    gemini calls work through terminal

---
+++ src/app/api/generate/route.js
@@ -0,0 +1,32 @@
+// Import `GoogleGenerative` from the package we installed earlier.
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { NextResponse } from "next/server";
+
+// Create an asynchronous function POST to handle POST 
+// request with parameters request and response.
+export async function POST(req, res) {
+
+    try {
+        // Access your API key by creating an instance of GoogleGenerativeAI 
+        const genAI = new GoogleGenerativeAI() // INSERT API KEY HERE
+
+        // Ininitalize the model
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
+
+        // Retrieve the data we recieve as part of the request body
+        const data = await req.json()
+
+        // Stores the prompt asked by the user
+        const prompt = data.body
+
+        // Pass the prompt to the model and retrieve the output
+        const result = await model.generateContent(prompt)
+        const response = await result.response;
+        const output = await response.text();
+
+        // Send the llm output as a server reponse object
+        return NextResponse.json({ output: output })
+    } catch (error) {
+        console.error(error)
+    }
+}
