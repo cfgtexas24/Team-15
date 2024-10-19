d5ab5de839b9087e4b06497967e32fb4c4d507ba
toastybridge <jtan208295@gmail.com>
2024-10-19 05:57:02 -0500

    Added job listing insights

--- src/app/joblisting/page.js
+++ src/app/joblisting/page.js
@@ -3,6 +3,7 @@ import JobListing from './JobListing'
 import { Button } from '@/components/ui/button'
 import { Sparkles } from 'lucide-react'
 import Link from 'next/link'
+import { Badge } from '@/components/ui/badge'
 
 function JobListingList() {
 
@@ -42,20 +43,36 @@ function JobListingList() {
   ]
 
   return (
-    <section className='flex flex-col h-screen w-full bg-gray-200 px-16 pt-16 gap-8'>
-      <div className='flex h-1/4 w-full bg-white p-8 rounded-xl'>
+    <section className='flex flex-col h-screen w-full bg-gray-200 px-16 pt-16 gap-8 overflow-hidden'>
+      <div className='flex flex-col h-1/4 gap-1'>
         <h3 className='text-gray-900 text-2xl'>Insights</h3>
+        <div className='flex h-full w-full bg-white p-8 rounded-xl gap-6'>
+          <div className='flex gap-2 items-center'>
+            <Badge className='bg-blue-500 text-xl'>187</Badge>
+            <p>Applicable Candidates</p>
+          </div>
+          <div className='flex gap-2 items-center'>
+            <Badge className='bg-purple-500 text-xl'>34</Badge>
+            <p>In Interview Progress</p>
+          </div>
+          <div className='flex gap-2 items-center'>
+            <Badge className='bg-green-500 text-xl'>17</Badge>
+            <p>Accepted Applications</p>
+          </div>
+
+        </div>
       </div>
-      <div className='flex flex-col h-full w-full bg-white p-8 rounded-t-xl gap-8 overflow-y-scroll no-scrollbar'>
-        <div className='flex justify-between'>
-          <h3 className='text-gray-900 text-2xl'>Listings</h3>
+      <div className='flex flex-col h-full gap-2'>
+        <h3 className='text-gray-900 text-2xl'>Listings</h3>
+        <div className='flex flex-col h-full w-full bg-white p-8 rounded-t-xl gap-8 overflow-y-scroll no-scrollbar'>
+
           <Button asChild className='bg-[#FFC10C]'>
             <Link href='/jobposting'><Sparkles />Create New Job Posting</Link>
           </Button>
+          {dummy.map((dumdum) =>
+            <JobListing key={dumdum.id} name={dumdum.name} location={dumdum.location} desc={desc} />
+          )}
         </div>
-        {dummy.map((dumdum) =>
-          <JobListing key={dumdum.id} name={dumdum.name} location={dumdum.location} desc={desc}/>
-        )}
       </div>
     </section>
   )
