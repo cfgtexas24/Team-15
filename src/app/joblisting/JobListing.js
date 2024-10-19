cee61b84b14c8530e928463c1083d018a8dff837
toastybridge <jtan208295@gmail.com>
2024-10-19 05:19:58 -0500

    Updated JobListing styling

--- src/app/joblisting/JobListing.js
+++ src/app/joblisting/JobListing.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
+import { BookOpen, Edit } from 'lucide-react'
 
 function JobListing(props) {
   return (
     <Link href="/">
-      <section className="w-full h-auto bg-white border border-gray-300 shadow-[0px_0px_1rem_rgba(0,0,0,0.1)] hover:shadow-[0px_0px_1rem_rgba(0,0,0,0.2)] transition rounded-2xl p-2">
-        <div className='flex items-end'>
-          <h3 className="text-gray-800 text-2xl">{props.name}{" "}</h3>
+      <section className="flex flex-col w-full h-auto bg-white border border-gray-300 shadow-[0px_0px_1rem_rgba(0,0,0,0.1)] hover:shadow-[0px_0px_1rem_rgba(0,0,0,0.2)] transition rounded-2xl p-4 gap-4">
+        <div className='flex items-center'>
+          <h3 className="text-gray-800 text-2xl whitespace-pre-wrap">{props.name} | </h3>
           <p className='text-gray-600'>{props.location}</p>
         </div>
-      <div className='flex'>
-        <Button>eoqwije</Button>
-      </div>
-
-
+        <div className='flex gap-3'>
+          <Button asChild className='bg-[#FFC10C]'>
+            <Link href='/applicable_candidates'><Edit />Edit Job Listing</Link>
+          </Button>
+          <Button asChild variant='outline' className='text-[#FFC10C]'>
+            <Link href='/applicable_candidates'><BookOpen />View Job Listing</Link>
+          </Button>
+        </div>
       </section>
     </Link>
   )
--- src/app/joblisting/page.js
+++ src/app/joblisting/page.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import JobListing from './JobListing'
 import { Button } from '@/components/ui/button'
-
+import { Sparkles } from 'lucide-react'
+import Link from 'next/link'
 
 function JobListingList() {
 
   const dummy = [
     {
       id: 1,
-      name: "Software Engineer",
+      name: "Software Engineer Intern",
       location: "1234 Windonway Lane"
 
     },
@@ -44,13 +45,15 @@ function JobListingList() {
       <div className='flex h-1/4 w-full bg-white p-8 rounded-xl'>
         <h3 className='text-gray-900 text-2xl'>Insights</h3>
       </div>
-      <div className='flex flex-col h-full w-full bg-white p-8 rounded-t-xl gap-8 overflow-y-scroll'>
+      <div className='flex flex-col h-full w-full bg-white p-8 rounded-t-xl gap-8 overflow-y-scroll no-scrollbar'>
         <div className='flex justify-between'>
           <h3 className='text-gray-900 text-2xl'>Listings</h3>
-          <Button className='bg-[#FFC10C] text-xl'>Create New Listing</Button>
+          <Button asChild className='bg-[#FFC10C]'>
+            <Link href='/jobposting'><Sparkles />Create New Job Posting</Link>
+          </Button>
         </div>
         {dummy.map((dumdum) =>
-          <JobListing key={dumdum.id} name={dumdum.name} location={dumdum.location}/>
+          <JobListing key={dumdum.id} name={dumdum.name} location={dumdum.location} />
         )}
       </div>
     </section>
