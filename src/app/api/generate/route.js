de6db4e78e299a39abc629917ef728c59b5becd1
Adeeb Momin <adeebmomin786@gmail.com>
2024-10-18 19:30:11 -0500

    added description for what gemini is to do

--- src/app/api/generate/route.js
+++ src/app/api/generate/route.js
@@ -4,11 +4,12 @@ import { NextResponse } from "next/server";
 
 // Create an asynchronous function POST to handle POST 
 // request with parameters request and response.
+
 export async function POST(req, res) {
 
     try {
         // Access your API key by creating an instance of GoogleGenerativeAI 
-        const genAI = new GoogleGenerativeAI() // INSERT API KEY HERE
+        const genAI = new GoogleGenerativeAI('') // INSERT API KEY HERE
 
         // Ininitalize the model
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
@@ -16,8 +17,9 @@ export async function POST(req, res) {
         // Retrieve the data we recieve as part of the request body
         const data = await req.json()
 
+        const context = "Assume you are a recruiter creating a job posting. I want you to include the job title, description (includes list of different qualifications), salary range, and if it is remote or in person (location). "
         // Stores the prompt asked by the user
-        const prompt = data.body
+        const prompt = context + data.body
 
         // Pass the prompt to the model and retrieve the output
         const result = await model.generateContent(prompt)
