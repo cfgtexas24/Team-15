04c6988abe80782ac52beadf038a210ae22a79e7
Adeeb Momin <adeebmomin786@gmail.com>
2024-10-19 04:18:39 -0500

    added basic calendar features

--- src/app/dashboard/calendar.jsx
+++ src/app/dashboard/calendar.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import DashboardLayout from '../dashboard/layout'; 
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -7,7 +8,7 @@ import interactionPlugin from '@fullcalendar/interaction';
 
 export default function Calendar() {
   const events = [
-    {title: 'Slot 1' , start: '2024-10-19T11:30:00', end: '2024-10-19T12:00:00'},
+    { title: 'Slot 1' , start: '2024-10-19T11:30:00', end: '2024-10-19T12:00:00'},
     { title: 'Slot 2', start: '2024-10-22T10:00:00', end: '2024-10-22T10:30:00' },
     { title: 'Slot 3', start: '2024-10-22T11:00:00', end: '2024-10-22T11:30:00' },
     { title: 'Slot 4', start: '2024-10-23T12:00:00', end: '2024-10-23T12:30:00' }
@@ -18,16 +19,18 @@ export default function Calendar() {
   };
 
   return (
-    <FullCalendar
-      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-      initialView="timeGridWeek"
-      selectable={true}
-      events={events}
-      eventClick={handleDateClick}
-      height="auto"
-      slotMinTime="09:00:00"
-      slotMaxTime="17:00:00"
-      allDaySlot={false}
-    />
+    <DashboardLayout>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="timeGridWeek"
+        selectable={true}
+        events={events}
+        eventClick={handleDateClick}
+        height="auto"
+        slotMinTime="09:00:00"
+        slotMaxTime="17:00:00"
+        allDaySlot={false}
+      />
+    </DashboardLayout>
   );
 }
