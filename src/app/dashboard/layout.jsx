210df8a186e4bf97037c6310854f193776ec5773
Adeeb Momin <adeebmomin786@gmail.com>
2024-10-19 05:40:02 -0500

    changes to nvbar

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -19,7 +19,7 @@ export default function DashboardLayout({ children }) {
   return (
     <div className="min-h-screen bg-gray-50">
       <nav className="navbar">
-        <Image src="/assets/images/package.png" className="logo" width={75} height={50} />
+        <Image src="/assets/images/mainLogo.png" className="logo" width={75} height={50} />
         <ul className="navOptions">
           {navItems.map((item) => (
             <Link key={item.href} href={item.href} className={ pathname === item.href ? 'text-white p-2 border border-black bg-[#475299]' : 'text-black p-2'}>{item.label}</Link>
