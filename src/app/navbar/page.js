9197124b30e3b1fef49e5502737221ba96a604ed
Adeeb Momin <adeebmomin786@gmail.com>
2024-10-18 21:30:14 -0500

    added navbar options

--- src/app/navbar/page.js
+++ src/app/navbar/page.js
@@ -4,7 +4,13 @@ import './style.css'
 export default function Navbar(){
     <nav classname="navbar">
         <img src="/assets/images/mainLogo.png" classname="logo"/>
-        <h2></h2>
+        <ul className="navOptions">
+            <li>Job Postings</li>
+            <li>Messages</li>
+            <li>Calendar</li>
+            <li>Inbox</li>
+            <li>Profile</li>
+        </ul>
     </nav>
 }
 
