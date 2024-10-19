6e6e8dece2986e9a18ad945df1cfce0cc3bcfea8
Adeeb Momin <adeebmomin786@gmail.com>
2024-10-18 23:03:07 -0500

    navbar fully displays

--- src/app/navbar/page.js
+++ src/app/navbar/page.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import Image from 'next/image'
 import './style.css'
+import React from 'react'
 
 export default function Navbar(){
-    <nav classname="navbar">
-        <img src="/assets/images/mainLogo.png" classname="logo"/>
-        <ul className="navOptions">
-            <li>Job Postings</li>
-            <li>Messages</li>
-            <li>Calendar</li>
-            <li>Inbox</li>
-            <li>Profile</li>
-        </ul>
-    </nav>
+    return(
+        <nav className="navbar">
+            <Image src="/assets/images/mainLogo.png" className="logo" width={50} height={50}/>
+            <ul className="navOptions">
+                <li>Job Postings</li>
+                <li>Messages</li>
+                <li>Calendar</li>
+                <li>Inbox</li>
+                <li>Profile</li>
+            </ul>
+        </nav>
+    )
 }
-
